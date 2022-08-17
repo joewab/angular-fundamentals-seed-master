4d809930fa17c9b56022a59a2acf107307ecf886
Joe AB <joe.anthonybrown@gmail.com>
2022-08-17 11:05:38 -0500

    working with binding and variables

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
-      Hello!
+      <div>
+        <button (click)="handleClick(username.value)">
+          Get value
+        </button>
+        <input type="text" #username>
+      </div>
+      <div>{{ name }}</div>
     </div>
-  `
+  `,
 })
 export class AppComponent {
+  name: string = 'Joe';
+  handleClick(value: string){
+   console.log(value);
+   
+  }
 
+  constructor() {
+
+  }
 }
