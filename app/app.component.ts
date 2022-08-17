5b3da359a72699a449191607b9e7769821839ec2
Joe AB <joe.anthonybrown@gmail.com>
2022-08-17 13:44:17 -0500

    two ways to use ngFor to loop through an array

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,61 @@
 import { Component } from "@angular/core";
 
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+}
+
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
-      <div>
-        <button (click)="handleClick(username.value)">
-          Get value
-        </button>
-        <input type="text" #username>
-      </div>
-      <div>{{ name }}</div>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+          <li>
+            {{ i }}: {{ passenger.fullname }}
+          </li>
+        </template>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index">
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
     </div>
   `,
 })
 export class AppComponent {
-  name: string = 'Joe';
-  handleClick(value: string){
-   console.log(value);
-   
-  }
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: "Stephen",
+      checkedIn: true,
+    },
+    {
+      id: 2,
+      fullname: "Rose",
+      checkedIn: false,
+    },
+    {
+      id: 3,
+      fullname: "Joe",
+      checkedIn: true,
+    },
+    {
+      id: 4,
+      fullname: "Louise",
+      checkedIn: true,
+    },
+    {
+      id: 5,
+      fullname: "Tine",
+      checkedIn: false,
+    },
+  ];
 
-  constructor() {
-
-  }
+  constructor() {}
 }
