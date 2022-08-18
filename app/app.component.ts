530518908c1d133e027db691826048e66c1ce8a5
Joe AB <joe.anthonybrown@gmail.com>
2022-08-18 15:49:02 -0500

    learned to construct a component and import it into the app, also to make a separate models folder for interfaces

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,81 +1,12 @@
 import { Component } from "@angular/core";
 
-interface Child {
-  name: string,
-  age: number
-}
-
-interface Passenger {
-  id: number;
-  fullname: string;
-  checkedIn: boolean;
-  checkedInDate: number | null;
-  children: Child[] | null;
-}
-
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: `
     <div class="app">
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span 
-          class="status"
-          [class.checked-in]="passenger.checkedIn"></span>
-          {{ i }}: {{ passenger.fullname }}
-          <p>{{ passenger | json }}</p>
-          <div class="date">
-            Check in date: 
-            {{ passenger.checkedInDate ? (passenger.checkedInDate | date: 'yMMMMd' | uppercase) : 'Not checked in' }}
-          </div>
-          <div class="children">
-          Children: {{ passenger.children?.length || 0}}
-          </div>
-        </li>
-      </ul>
+     <passenger-dashboard></passenger-dashboard>
     </div>
   `,
 })
-export class AppComponent {
-  passengers: Passenger[] = [
-    {
-      id: 1,
-      fullname: "Stephen",
-      checkedIn: true,
-      checkedInDate: 1490742000000,
-      children: null,
-    },
-    {
-      id: 2,
-      fullname: "Rose",
-      checkedIn: false,
-      checkedInDate: null,
-      children: [{name: 'Ted', age: 12}, {name: 'Mary', age: 4}]
-    },
-    {
-      id: 3,
-      fullname: "Joe",
-      checkedIn: true,
-      checkedInDate: 1491606000000,
-      children: null, 
-    },
-    {
-      id: 4,
-      fullname: "Louise",
-      checkedIn: true,
-      checkedInDate: 1488412800000,
-      children: [{name:'Jessica', age: 7}], 
-    },
-    {
-      id: 5,
-      fullname: "Tine",
-      checkedIn: false,
-      checkedInDate: null,
-      children: null,
-    },
-  ];
-
-  constructor() {}
-}
+export class AppComponent {}
---
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -0,0 +1,73 @@
+import { Component } from "@angular/core";
+
+import { Passenger } from "../../models/passenger.interface";
+
+@Component({
+  selector: "passenger-dashboard",
+  styleUrls: ["passenger-dashboard.component.scss"],
+  template: `
+    <div>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index">
+          <span class="status" [class.checked-in]="passenger.checkedIn"></span>
+          {{ i }}: {{ passenger.fullname }}
+          <p>{{ passenger | json }}</p>
+          <div class="date">
+            Check in date:
+            {{
+              passenger.checkedInDate
+                ? (passenger.checkedInDate | date: "yMMMMd" | uppercase)
+                : "Not checked in"
+            }}
+          </div>
+          <div class="children">
+            Children: {{ passenger.children?.length || 0 }}
+          </div>
+        </li>
+      </ul>
+    </div>
+  `,
+})
+export class PassengerDashboardComponent {
+    passengers: Passenger[] = [
+        {
+          id: 1,
+          fullname: "Stephen",
+          checkedIn: true,
+          checkedInDate: 1490742000000,
+          children: null,
+        },
+        {
+          id: 2,
+          fullname: "Rose",
+          checkedIn: false,
+          checkedInDate: null,
+          children: [{name: 'Ted', age: 12}, {name: 'Mary', age: 4}]
+        },
+        {
+          id: 3,
+          fullname: "Joe",
+          checkedIn: true,
+          checkedInDate: 1491606000000,
+          children: null, 
+        },
+        {
+          id: 4,
+          fullname: "Louise",
+          checkedIn: true,
+          checkedInDate: 1488412800000,
+          children: [{name:'Jessica', age: 7}], 
+        },
+        {
+          id: 5,
+          fullname: "Tine",
+          checkedIn: false,
+          checkedInDate: null,
+          children: null,
+        },
+      ];
+    
+      constructor() {}
+
+}
