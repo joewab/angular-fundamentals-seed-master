4836cebe9231d888f47585da1c198358975361f0
Joe AB <joe.anthonybrown@gmail.com>
2022-08-22 15:55:42 -0500

    created inputs using ngModel that use an initial value based on the data

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -2,21 +2,27 @@ import { Component, Input } from "@angular/core";
 
 import { Passenger } from "../../models/passenger.interface";
 
-
 @Component({
-    selector: 'passenger-form',
-    styleUrls: ['passenger-form.component.scss'],
-    template: `
-        <form>
-        Form!
-        <div>
-        {{ detail | json }}
-        </div>
-        </form>
-    `
-})
+  selector: "passenger-form",
+  styleUrls: ["passenger-form.component.scss"],
+  template: `
+    <form #form="ngForm" novalidate>
+      {{ detail | json }}
+      <div>
+        Passenger name:
+        <input tyoe="text" name="fullname" [ngModel]="detail?.fullname" />
+      </div>
 
+      <div>
+        Passenger ID:
+        <input tyoe="text" name="id" [ngModel]="detail?.id" />
+      </div>
+
+      {{ form.value | json }}
+    </form>
+  `,
+})
 export class PassengerFormComponent {
-    @Input()
-    detail: Passenger;
+  @Input()
+  detail: Passenger;
 }
