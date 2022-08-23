aa2ba31f7fe0b252a49136dafb233cf582bc7c3d
Joe AB <joe.anthonybrown@gmail.com>
2022-08-23 10:03:48 -0500

    learning routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
- 
+import { RouterModule } from '@angular/router';
+
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 import { AppComponent } from './app.component';
 
