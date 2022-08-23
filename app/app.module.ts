0833d03312896cb5eae15e0d5e2aef62d2c29a96
Joe AB <joe.anthonybrown@gmail.com>
2022-08-23 10:28:31 -0500

    added wildcard component for not found

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,15 +6,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { PassengerDashboardModule } from './passenger-dashboard/passenger-dashboard.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, pathMatch: 'full' }
-]
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     //angular modules
---
+++ app/not-found.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'not-found',
+    template: `
+        <div>
+            Not found
+        </div>
+    `
+})
+
+export class NotFoundComponent {
+    
+}
